2c560de86010a173bda857b3f40107e867aadfa8
Chris Hodgson <chris.hodgson@pimberly.com>
2022-04-25 08:02:54 +0100

    update mobile calendar src

--- pages/calendar.js
+++ pages/calendar.js
@@ -19,7 +19,7 @@ export default function Calendar() {
       <div className="iframe-mobile">
         <iframe
           title="mobile"
-          src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FLondon&showTitle=0&showDate=0&showPrint=0&showTz=0&mode=AGENDA&hl=en_GB&src=NjVoaTZubzJzZGs2bnYybGthMjI2dDU0ajhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23D50000&color=%233F51B5&color=%2333B679&color=%23F6BF26&color=%238E24AA&color=%237CB342&color=%23B39DDB&color=%23616161&color=%23E67C73"
+          src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FLondon&showTitle=0&showDate=0&showPrint=0&showTz=0&mode=AGENDA&hl=en_GB&src=NjVoaTZubzJzZGs2bnYybGthMjI2dDU0ajhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23E67C73"
           style={{ width: '100%', height: '600px', display: 'block' }}
         ></iframe>
       </div>
