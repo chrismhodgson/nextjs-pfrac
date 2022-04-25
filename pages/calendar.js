a1131cada582622c244f1da4fb06303e840ce06f
Chris Hodgson <chris.hodgson@pimberly.com>
2022-04-25 08:01:22 +0100

    update mobile calendar src

--- pages/calendar.js
+++ pages/calendar.js
@@ -19,7 +19,7 @@ export default function Calendar() {
       <div className="iframe-mobile">
         <iframe
           title="mobile"
-          src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FLondon&showTitle=0&showDate=0&showPrint=0&showTz=0&mode=AGENDA&hl=en_GB&src=NjVoaTZubzJzZGs2bnYybGthMjI2dDU0ajhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=Y2hyaXNob2Rnc29ud2ViLmNvLnVrX2w1OTF0cTluNWU2dnY0cGdvZGNiMDZjOG1zQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=Y2hyaXNob2Rnc29ud2ViLmNvLnVrX3E0YWRodTBrZG11azFlODhjaHBjcmxxNHQ4QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&src=Y2hyaXNob2Rnc29ud2ViLmNvLnVrX2VxYzh0Mzk0bWo3c2ltZDA3bXJzcHVmNmJjQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&src=Y2hyaXNob2Rnc29ud2ViLmNvLnVrX2kyaWJ2anRqNmRzampzOTg1OTg1YmU5djRzQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&src=Y2hyaXNob2Rnc29ud2ViLmNvLnVrX2Y0Z2w0MWZvcmNscDIzNzJkYzdsMXBrMzFvQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&src=Y2hyaXNob2Rnc29ud2ViLmNvLnVrX3ZuaGVidnUzN3U1ZWFmMWlyOHMxNzltNzBvQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&src=dHJhY3lqaG9kZ3NvbkBnbWFpbC5jb20&color=%23D50000&color=%233F51B5&color=%2333B679&color=%23F6BF26&color=%238E24AA&color=%237CB342&color=%23B39DDB&color=%23616161&color=%23E67C73"
+          src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FLondon&showTitle=0&showDate=0&showPrint=0&showTz=0&mode=AGENDA&hl=en_GB&src=NjVoaTZubzJzZGs2bnYybGthMjI2dDU0ajhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23D50000&color=%233F51B5&color=%2333B679&color=%23F6BF26&color=%238E24AA&color=%237CB342&color=%23B39DDB&color=%23616161&color=%23E67C73"
           style={{ width: '100%', height: '600px', display: 'block' }}
         ></iframe>
       </div>
