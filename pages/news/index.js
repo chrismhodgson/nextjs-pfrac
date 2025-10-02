aeabf5a7a910f30b0342bccdea213c74fbacb3e0
Chris.hodgson <chris.hodgson@pimberly.com>
2025-10-02 18:35:16 +0100

    new changes

--- pages/news/index.js
+++ pages/news/index.js
@@ -3,7 +3,7 @@ import siteMetadata from '@/data/siteMetadata'
 import ListLayout from '@/layouts/ListLayout'
 import { PageSEO } from '@/components/SEO'
 
-export const POSTS_PER_PAGE = 5
+export const POSTS_PER_PAGE = 15
 export const POSTS_FOLDER = '_articles'
 
 export async function getStaticProps() {
